a952d1300ba0ebc2ca2ed44fdc1fc111d54d2841
Nick <nick@Mac.lan>
2024-11-04 22:26:12 -0500

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,33 +6,43 @@ import Footer from '../Footer/Footer'
 import Preloader from '../Preloader/Preloader'
 import About from '../About/About'
 import Picker from '../Picker/Picker'
+import activeModalContext from '../../utils/activeModal'
 
 function App() {
 
   const [next, setNext] = useState(false);
   const [showPicker, setShowPicker] = useState(false);
-
+  const [activeModal, setActiveModal] = useState(null);
   useEffect(() => {
     if(next === true){
-      setTimeout(() => {setShowPicker(true)}, 300)
+      setTimeout(() => {setShowPicker(true)}, 500)
     }
   }, [next])
 
+  const closeModal = () => setActiveModal("");
+
+
+  useEffect(() => console.log(activeModal), [activeModal])
   return (
-    <>
+    <activeModalContext.Provider value={{activeModal, setActiveModal, closeModal}}>
       <div className='app'>
           <Header/>
+          {activeModal === "about" && (
+            <div className="app__about">
+              <About/>
+            </div>
+          )}
+          {!showPicker && (
           <div className="app__main">
             <Main setNext={setNext} next={next}/>
-          </div>
+          </div>)}
           {
             showPicker && (
               <Picker/>
             )
           }
-         
       </div>
-    </>
+    </activeModalContext.Provider>
   )
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import './Navigation.css'
+import activeModalContext from '../../utils/activeModal'
 
 function Navigation() {
 
+  const {activeModal, setActiveModal} = React.useContext(activeModalContext);
+
+  const handleAbout = () => {
+    setActiveModal("about");
+  }
   return (
     <>
       <div className='navigation'>
         <ul className='navigation__list'>
             <li className='navigation__item'>LOGIN TO STEAM</li>
-            <li className='navigation__item'>ABOUT</li>
+            <li className='navigation__item' onClick={() => handleAbout()}>ABOUT</li>
             <li className='navigation__item'>GITHUB</li>
     
         </ul>
